31740618ccca22f71f7f9fc59db53289c23de837
olenazubashenko <olenazubashenko@gmail.com>
2019-11-27 09:58:21 +0200

    Updates

---
+++ precache-manifest.0fe088b6fa08a23e9407787a7d8c3bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11c7c390da70e90df76d7da6adbfea6",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "19a62dadcb1c2cb5a9c3",
+    "url": "/todo-react/static/css/main.853c9009.chunk.css"
+  },
+  {
+    "revision": "59a2ea5100e17473b639",
+    "url": "/todo-react/static/js/2.c77de63f.chunk.js"
+  },
+  {
+    "revision": "19a62dadcb1c2cb5a9c3",
+    "url": "/todo-react/static/js/main.913e8402.chunk.js"
+  },
+  {
+    "revision": "2e9fc746d4cac6425068",
+    "url": "/todo-react/static/js/runtime-main.383685b7.js"
+  }
+]);
